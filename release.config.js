f0af1baa33e0c340d664bafc2f4dd7c66c19abba
Nikith13 <mogallapallinagavenkateswara.mogallapalli@anko.com>
2025-01-22 10:59:05 +0530

    ci: Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,18 +1,34 @@
-//release config file
 module.exports = {
-    branches: ['main'],
-    tagFormat: 'v${version}',
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      [
-        '@semantic-release/release-notes-generator',
-        {
-          writerOpts: {
-            headerPartial: '## Changes in version {{version}}',
-          },
+  branches: ['mmogalla/pull_request_file'],
+  tagFormat: 'v${version}',
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventionalcommits",
+        "releaseRules": [
+          { "type": "feat", "release": "minor" },
+          { "type": "fix", "release": "patch" },
+          { "type": "chore", "release": "patch" },
+          { "type": "ci", "release": "patch" },
+          { "type": "docs", "release": "patch" },
+          { "type": "style", "release": "patch" },
+          { "type": "refactor", "release": "patch" },
+          { "type": "perf", "release": "patch" },
+          { "type": "test", "release": "patch" }
+        ]
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        writerOpts: {
+          headerPartial: '## Changes in version {{version}}',
         },
-      ],      
-      '@semantic-release/github',     
-      '@semantic-release/git',
-    ]
-  };
+      },
+    ],
+    '@semantic-release/changelog',      
+    '@semantic-release/github',     
+    '@semantic-release/git',
+  ]
+};
