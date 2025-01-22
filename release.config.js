af9d28e017bf224aff01dca0ea582f14aaeb6996
Nikith13 <mogallapallinagavenkateswara.mogallapalli@anko.com>
2025-01-22 11:04:07 +0530

    fix: Update release.config.js

--- release.config.js
+++ release.config.js
@@ -2,23 +2,7 @@ module.exports = {
   branches: ['mmogalla/pull_request_file'],
   tagFormat: 'v${version}',
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        "preset": "conventionalcommits",
-        "releaseRules": [
-          { "type": "feat", "release": "minor" },
-          { "type": "fix", "release": "patch" },
-          { "type": "chore", "release": "patch" },
-          { "type": "ci", "release": "patch" },
-          { "type": "docs", "release": "patch" },
-          { "type": "style", "release": "patch" },
-          { "type": "refactor", "release": "patch" },
-          { "type": "perf", "release": "patch" },
-          { "type": "test", "release": "patch" }
-        ]
-      }
-    ],
+    '@semantic-release/commit-analyzer',
     [
       '@semantic-release/release-notes-generator',
       {
