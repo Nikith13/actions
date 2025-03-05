a8ffe3f1443b6a10d6bd1443dbdf4450ecc55ef6
Nikith13 <mogallapallinagavenkateswara.mogallapalli@anko.com>
2025-03-05 15:38:54 +0530

    chore: Create re.yaml (#16)

--- release.config.js
+++ release.config.js
@@ -1,35 +1,35 @@
-//release.config.js file
-module.exports = {
-  branches: ['main'],
-  tagFormat: 'v${version}',
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        "preset": "conventionalcommits",
-        "releaseRules": [
-          { "type": "feat", "release": "minor" },
-          { "type": "fix", "release": "patch" },
-          { "type": "chore", "release": "patch" },
-          { "type": "ci", "release": "patch" },
-          { "type": "docs", "release": "patch" },
-          { "type": "style", "release": "patch" },
-          { "type": "refactor", "release": "patch" },
-          { "type": "perf", "release": "patch" },
-          { "type": "test", "release": "patch" }
-        ]
-      }
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        writerOpts: {
-          headerPartial: '## Changes in version {{version}}',
-        },
-      },
-    ],
-    '@semantic-release/changelog',      
-    '@semantic-release/github',     
-    '@semantic-release/git',
-  ]
-};
+// //release.config.js file
+// module.exports = {
+//   branches: ['main'],
+//   tagFormat: 'v${version}',
+//   plugins: [
+//     [
+//       "@semantic-release/commit-analyzer",
+//       {
+//         "preset": "conventionalcommits",
+//         "releaseRules": [
+//           { "type": "feat", "release": "minor" },
+//           { "type": "fix", "release": "patch" },
+//           { "type": "chore", "release": "patch" },
+//           { "type": "ci", "release": "patch" },
+//           { "type": "docs", "release": "patch" },
+//           { "type": "style", "release": "patch" },
+//           { "type": "refactor", "release": "patch" },
+//           { "type": "perf", "release": "patch" },
+//           { "type": "test", "release": "patch" }
+//         ]
+//       }
+//     ],
+//     [
+//       '@semantic-release/release-notes-generator',
+//       {
+//         writerOpts: {
+//           headerPartial: '## Changes in version {{version}}',
+//         },
+//       },
+//     ],
+//     '@semantic-release/changelog',      
+//     '@semantic-release/github',     
+//     '@semantic-release/git',
+//   ]
+// };
